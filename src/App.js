82144d1d3d8ec2e77f05c29697f177f888c798bf
Chris Guy <christien.guy@gmail.com>
2018-07-18 11:09:59 +0100

    added nav menu animation and dropdown menu transition

--- src/App.js
+++ src/App.js
@@ -3,26 +3,48 @@ import styled from "styled-components";
 import WithPose from "./WithPose";
 import WithMotion from "./WithMotion";
 import WithSpring from "./WithSpring";
+import posed from "../node_modules/react-pose";
 
 const AppWrapper = styled.div`
   display: grid;
   grid-template-rows: 80px 1fr 100px;
 `;
 const Nav = styled.nav`
-  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  background-color: #17c7b6;
+  width: 300px;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  padding: 32px 16px 16px;
+
+  z-index: 100;
 `;
+
+const PosedNav = posed(Nav)({
+  closed: {
+    x: "-100%",
+    delay: 150,
+    staggerChildren: 50
+  },
+  open: {
+    x: "0%",
+    delayChildren: 200,
+    staggerChildren: 50
+  },
+  initialPose: "closed"
+});
 
 const NavButton = styled.input`
   transition: box-shadow 0.2s ease-out;
-  height: 100%;
-  width: 64px;
+  height: 40px;
+  width: 100%;
   background-color: #fff;
   border: 0;
   box-shadow: inset 0 0 0 1px #000;
-  margin: 0 16px;
+  margin: 16px 0;
 
   &:hover {
     box-shadow: inset 0 0 0 6px #000;
@@ -30,15 +52,39 @@ const NavButton = styled.input`
   }
 `;
 
+const PosedNavButton = posed(NavButton)({
+  closed: {
+    y: -20,
+    opacity: 0
+  },
+  open: {
+    y: 0,
+    opacity: 1
+  },
+  initialPose: "closed"
+});
+
 const Wrapper = styled.div`
   padding: 120px;
   display: grid;
   grid-template-rows: 1fr 1fr 1fr;
 `;
 
+const NavToggleButton = styled.button`
+  background-color: #fff;
+  border: 1px solid black;
+  outline: none;
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  left: 16px;
+  top: 16px;
+`;
+
 class App extends Component {
   state = {
-    showing: "pose"
+    showing: "pose",
+    navState: "closed"
   };
 
   children = () => {
@@ -56,18 +102,40 @@ class App extends Component {
 
   onNavigate = e => {
     this.setState({
-      showing: e.target.value
+      showing: e.target.value,
+      navState: "closed"
     });
   };
 
+  toggleNav = () => {
+    this.setState(state => ({
+      navState: state.navState === "closed" ? "open" : "closed"
+    }));
+  };
+
   render() {
+    const { navState } = this.state;
     return (
       <AppWrapper>
-        <Nav>
-          <NavButton onClick={this.onNavigate} value="pose" type="button" />
-          <NavButton onClick={this.onNavigate} value="motion" type="button" />
-          <NavButton onClick={this.onNavigate} value="spring" type="button" />
-        </Nav>
+        <NavToggleButton onClick={this.toggleNav} />
+        <PosedNav pose={navState}>
+          <NavToggleButton onClick={this.toggleNav} withParent={false} />
+          <PosedNavButton
+            onClick={this.onNavigate}
+            value="pose"
+            type="button"
+          />
+          <PosedNavButton
+            onClick={this.onNavigate}
+            value="motion"
+            type="button"
+          />
+          <PosedNavButton
+            onClick={this.onNavigate}
+            value="spring"
+            type="button"
+          />
+        </PosedNav>
         <Wrapper>{this.children()}</Wrapper>
       </AppWrapper>
     );
--- src/WithPose/Box.jsx
+++ src/WithPose/Box.jsx
@@ -1,25 +1,58 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
+import posed from "../../node_modules/react-pose";
 
 const StyledBox = styled.div`
   width: 300px;
   height: 100px;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
-  padding: 16px;
+  /* padding: 16px; */
 `;
 
+const PosedBox = posed(StyledBox)({
+  closed: {
+    width: 0,
+    height: 0,
+    delay: 100
+  },
+  open: {
+    width: "300px",
+    height: "100px",
+    delayChildren: 100
+  },
+  initialPose: "closed"
+});
+
+const Content = posed.div({
+  closed: {
+    opacity: 0,
+    y: 20
+  },
+  open: {
+    opacity: 1,
+    y: 0
+  },
+  initialPose: "closed"
+});
+
 class Box extends Component {
+  state = {
+    boxState: "closed"
+  };
   triggerFade = () => {
-    console.log("====================================");
-    console.log("FADING");
-    console.log("====================================");
+    this.setState(state => ({
+      boxState: state.boxState === "open" ? "closed" : "open"
+    }));
   };
 
   render() {
+    const { boxState } = this.state;
     return (
       <Fragment>
-        <StyledBox>This is some content</StyledBox>
         <button onClick={this.triggerFade}>fade</button>
+        <PosedBox pose={boxState}>
+          <Content>This is some content</Content>
+        </PosedBox>
       </Fragment>
     );
   }
